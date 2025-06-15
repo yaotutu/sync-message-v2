dff3cfe19142e9e3d2f08d5f591b1463b50506d6
yaotutu <957810668@qq.com>
2025-06-15 12:00:03 +0800

    新增安装文档和使用教程，整理项目结构；更新 next.config.js 配置格式

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        // !! 仅在生产环境构建时禁用类型检查
-        ignoreBuildErrors: true,
-    },
-    // 确保所有 API 路由都是动态的
-    async headers() {
-        return [
-            {
-                source: '/api/:path*',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-store',
-                    },
-                ],
-            },
-        ]
-    },
-}
+  allowedDevOrigins: ['192.168.55.100'],
+  typescript: {
+    // !! 仅在生产环境构建时禁用类型检查
+    ignoreBuildErrors: true,
+  },
+  // 确保所有 API 路由都是动态的
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
